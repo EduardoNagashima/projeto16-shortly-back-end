b316eeab969317c9395e19b5778c147774d665d5
Eduardo Nagashima <eduyudji@gmail.com>
2022-06-10 16:29:38 -0300

    heroku

--- tables.sql
+++ tables.sql
@@ -1,28 +1,30 @@
-CREATE DATABASE projeto16
-
-CREATE TABLE users (
-    "id" serial NOT NULL,
-    "name" varchar(255) NOT NULL,
-    "email" varchar(255) NOT NULL UNIQUE,
-    "password" varchar(255) NOT NULL,
-    "createdAt" timestamp NOT NULL DEFAULT NOW(),
-    CONSTRAINT users_pk PRIMARY KEY (id)
-);
-
-CREATE TABLE "sessions" (
-    "id" serial NOT NULL,
-    "token" text NOT NULL,
-    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
-    "userId" integer REFERENCES users(id),
-    CONSTRAINT sessions_pk PRIMARY KEY (id)
+CREATE TABLE "users" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL UNIQUE,
+	"password" varchar(255) NOT NULL,
+	"createdAt" DATE NOT NULL DEFAULT 'now()',
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
 );
 
 CREATE TABLE "usersLinks" (
-    "id" serial NOT NULL,
-    "fullLink" TEXT NOT NULL,
-    "shortLink" TEXT NOT NULL,
-    "userId" integer REFERENCES users(id),
-    "views" integer NOT NULL DEFAULT '0',
-    "createdAt" timestamp NOT NULL DEFAULT NOW(),
-    CONSTRAINT "usersLinks_pk" PRIMARY KEY (id)
+	"id" serial NOT NULL,
+	"fullLink" TEXT NOT NULL,
+	"shortLink" TEXT NOT NULL,
+	"userId" int NOT NULL,
+	"views" int NOT NULL DEFAULT '0',
+	"createdAt" DATE NOT NULL DEFAULT 'now()',
+	CONSTRAINT "usersLinks_pk" PRIMARY KEY ("id")
 );
+
+
+CREATE TABLE "sessions" (
+	"id" serial NOT NULL,
+	"token" TEXT NOT NULL,
+	"createdAt" DATE NOT NULL DEFAULT 'now()',
+	"userId" int NOT NULL,
+	CONSTRAINT "sessions_pk" PRIMARY KEY ("id")
+);
+
+ALTER TABLE "usersLinks" ADD CONSTRAINT "usersLinks_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
